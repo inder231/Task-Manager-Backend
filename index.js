396512d825c8eda274d2db54e642daf9e66ac97b
Inder-Pal-github <inder39811@gmail.com>
2023-05-22 21:24:52 +0530

    Fix: trying to add domain to check cookies

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get(
       httpOnly: true,
       sameSite: "Lax",
       signed: true,
-      domain:".task-manager-frontend-two.vercel.app"
+      domain:"https://task-manager-frontend-two.vercel.app"
     });
     res.sendStatus(200);
   }
