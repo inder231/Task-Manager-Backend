f70f5c7131f70fe9edb7f404eb0d3fec270153d6
Inder-Pal-github <inder39811@gmail.com>
2023-05-26 11:07:16 +0530

    checking

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,8 +5,8 @@ const { redis } = require("../config/redis");
 module.exports = {
   auth: async (req, res, next) => {
     try {
-      // console.log(req.cookies);
-      // console.log(req.signedCookies,"signed cookies");
+      console.log(req.cookies);
+      console.log(req.signedCookies,"signed cookies");
       const access_token = req?.cookies?.access_token || req?.signedCookies?.access_token
       
       if (!access_token) throw createError.Unauthorized();
